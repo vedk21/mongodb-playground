90b1c0446a3dd4f268a04590825c985cf7daf24f
vedk21 <vedantkakade.cp@gmail.com>
2020-05-11 23:52:13 +0530

    Added: CRUD - update - array - matched array documents only

---
+++ Intermediate/CRUD/update/array/update_matched_array_documents.js
@@ -0,0 +1,29 @@
+use champions_league;
+
+// If we want to update the documents inside an array for which the condition is matched, we use '$' operator like follows
+db.teams.updateMany(
+  {
+    "matches.is_interview_scheduled": true
+  },
+  {
+    $set: {
+      "matches.$": {
+        day: '27-09-2020',
+        scheduled: true
+      } // this overwrites the complete document which was matched
+    }
+  }
+);
+
+db.teams.updateMany(
+  {
+    "matches.is_interview_scheduled": true
+  },
+  {
+    $set: {
+      "matches.$.lucky_draw": false // this adds new field to document which was matched
+    }
+  }
+);
+
+// NOTE: $ works as a placeholder - representing the matched document inside an array
