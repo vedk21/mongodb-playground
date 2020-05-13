943766b9096da8bdd6945a085d2e9276ae8da365
vedk21 <vedantkakade.cp@gmail.com>
2020-05-14 00:20:01 +0530

    Modified: Indexes - new parameters to explain method (queryPlanner, allPlansExecution)

--- Advanced/indexes/execution_stats_for_query.js
+++ Advanced/indexes/execution_stats_for_query.js
@@ -17,3 +17,22 @@ db.contacts.explain('executionStats').find(
     }
   }
 );
+
+// queryPlanner returns basic stats regarding execution of query
+db.contacts.explain('queryPlanner').find(
+  {
+    "dob.age": {
+      $gt: 70
+    }
+  }
+);
+
+// allPlansExecution returns detailed stats regarding execution of query
+// this returns even the rejected plans stats, showing if they were run, what would have been the stats
+db.contacts.explain('allPlansExecution').find(
+  {
+    "dob.age": {
+      $gt: 70
+    }
+  }
+);
